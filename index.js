3331aafe6c695d8518d3c71f5ed31cc7eb0bc545
Sarah Bass <69780815+SarahBass@users.noreply.github.com>
2023-03-07 18:36:24 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var buttonnumber=0;
 var button = document.getElementById("clickme")
+const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+
+const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
 setInterval(() => {
     
     d = new Date(); //object of date()
@@ -8,10 +14,33 @@ setInterval(() => {
     sec = d.getSeconds();
     month = d.getMonth();
     day = d.getDate();
+    year = d.getFullYear();
+    week = d.getDay();
     astro = getHoroscope();
     
+
+
+
     function getHoroscope() {
     return "Aries";
+    }
+    
+    
+    function getAlmanac(){
+    
+     if (month == 0){ return "Wolf Moon";}
+     
+    else if (month == 1){ return "Snow Moon";}
+    else if (month == 2){ return "Worm Moon";}
+    else if (month == 3){ return " Pink Moon";}
+    else if (month == 4){ return "Flower Moon";}
+    else if (month == 5){ return "Strawberry Moon";}
+    else if (month == 6){ return "Buck Moon";}
+    else if (month == 7){ return "Sturgeon Moon";}
+    else if (month == 8){ return "Corn Moon";}
+    else if (month == 9){ return "Harvest Moon";}
+    else if (month == 10){ return "Beaver Moon";}
+    else if (month == 11){ return "Cold Moon";}else {return "Moon";}
     }
     
     hr_rotation = 30 * hr + min / 2; //converting current time
@@ -22,9 +51,9 @@ setInterval(() => {
 
 button.onclick = function() {
   buttonnumber += 1;
-  if (buttonnumber ==1){button.innerHTML = "Moon Almanac";}
-  else if (buttonnumber ==2){button.innerHTML = "Month Zodiac";}
-  else{button.innerHTML = "Try it!";}
+  if (buttonnumber ==1){button.innerHTML = "Almanac: "+ getAlmanac();}
+  else if (buttonnumber ==2){button.innerHTML = "Zodiac: "+getHoroscope();}
+  else{button.innerHTML = "Clock Showing : "+ dayNames[week]+ " , "+ monthNames[month] + " "+ day + ", "+ year + "  Time Stamp : <"+ hr + ":"+ min+ ":"+sec+">" ;}
 };
 
     if (buttonnumber == 1){star.style.backgroundImage= "url('https://github.com/SarahBass/SarahBass.github.io/blob/main/images/"+ month+ ".png?raw=true')";}
