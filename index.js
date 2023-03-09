f12c5dded46b882ffbad929c2a6d4b811d3b2935
Sarah Bass <69780815+SarahBass@users.noreply.github.com>
2023-03-08 17:40:55 -0800

    Update index.js

--- index.js
+++ index.js
@@ -497,6 +497,177 @@ window.onload = function() {
       }
     }
 
+
+
+    function planetnews() {
+      if (year == 2022 && month == 0) {
+        return "Mercury Visible at Sunset until 30th";
+      } else if (year == 2022 && month == 1) {
+        return "Venus Brightest on February 9";
+      } else if (year == 2022 && month == 2) {
+        return "Spring Equinox on March 20th";
+      } else if (year == 2022 && month == 3) {
+        return "Mercury will be visible at Sunrise until 30th";
+      } else if (year == 2022 && month == 4) {
+        return "May 6: Eta Aquarid Meteors";
+      } else if (year == 2022 && month == 5) {
+        return "June 20: Summer Solstice";
+      } else if (year == 2022 && month == 6) {
+        return "Pluto at Opposition Jul 20";
+      } else if (year == 2022 && month == 7) {
+        return "August 24 : Uranus begins retrograde motion";
+      } else if (year == 2022 && month == 8) {
+        return "Fall Equinox September 22nd";
+      } else if (year == 2022 && month == 9) {
+        return "Mars in Retrograde October 30";
+      } else if (year == 2022 && month == 10) {
+        return "Uranus at opposition – November 9";
+      } else if (year == 2022 && month == 11) {
+        return "Dec 21: Winter Solstice";
+      } else if (year == 2023 && month == 0) {
+        return "Jan 7: Inferior conjunction Mercury";
+      } else if (year == 2023 && month == 1) {
+        return "February 16: Saturn in conjunction with the sun";
+      } else if (year == 2023 && month == 2) {
+        return "Spring Equinox on March 20th";
+      } else if (year == 2023 && month == 3) {
+        return "April 11: Jupiter at solar conjunction";
+      } else if (year == 2023 && month == 4) {
+        return "Mercury Visible at Sunrise until 30th";
+      } else if (year == 2023 && month == 5) {
+        return "June 21: Summer Solstice";
+      } else if (year == 2023 && month == 6) {
+        return "Pluto at Opposition : July 22 ";
+      } else if (year == 2023 && month == 7) {
+        return "August 13th: Perseid Meteor Shower";
+      } else if (year == 2023 && month == 8) {
+        return "Fall Equinox September 23rd";
+      } else if (year == 2023 && month == 9) {
+        return "October 22 - Orionids Meteor Shower";
+      } else if (year == 2023 && month == 10) {
+        return "Uranus at opposition – November 13";
+      } else if (year == 2023 && month == 11) {
+        return "Winter Solstice on Dec 21st";
+      } else if (year == 2024 && month == 0) {
+        return "January 4 - Quadrantids Meteor Shower";
+      } else if (year == 2024 && month == 1) {
+        return "Centaurid Meteor Shower";
+      } else if (year == 2024 && month == 2) {
+        return "Spring Equinox March 19th";
+      } else if (year == 2024 && month == 3) {
+        return "April 8: Total Solar Eclipse parts of USA";
+      } else if (year == 2024 && month == 4) {
+        return "May 7 - Eta Aquarids Meteor Shower";
+      } else if (year == 2024 && month == 5) {
+        return "Summer Solstice Jun 20th";
+      } else if (year == 2024 && month == 6) {
+        return "Mercury visible at Sunset until 30th";
+      } else if (year == 2024 && month == 7) {
+        return "August 13 - Perseids Meteor Shower";
+      } else if (year == 2024 && month == 8) {
+        return "Fall Equinox September 24";
+      } else if (year == 2024 && month == 9) {
+        return "October 22 - Orionids Meteor Shower";
+      } else if (year == 2024 && month == 10) {
+        return "Mercury visible at Sunset until 30th";
+      } else if (year == 2024 && month == 11) {
+        return "Winter Solstice Dec 21st";
+      } else if (year == 2025 && month == 0) {
+        return "Mars at Opposition on January 16th";
+      } else if (year == 2025 && month == 1) {
+        return "Venus at greatest brightness: Feb 16";
+      } else if (year == 2025 && month == 2) {
+        return "Spring Equinox March 20th";
+      } else if (year == 2025 && month == 3) {
+        return "April 23rd - Lyrids Meteor Shower";
+      } else if (year == 2025 && month == 4) {
+        return "May 7th - Eta Aquarids Meteor Shower";
+      } else if (year == 2025 && month == 5) {
+        return "Summer Solstice June 20th";
+      } else if (year == 2025 && month == 6) {
+        return "July 29th - Delta Aquarids Meteor Shower";
+      } else if (year == 2025 && month == 7) {
+        return "August 13th - Perseids Meteor Shower";
+      } else if (year == 2025 && month == 8) {
+        return "Fall Equinox: September 22nd";
+      } else if (year == 2025 && month == 9) {
+        return "October 22nd - Orionids Meteor Shower";
+      } else if (year == 2025 && month == 10) {
+        return "November 5th - Taurids Meteor Shower";
+      } else if (year == 2025 && month == 11) {
+        return "Winter Solstice : December 21st";
+      } else if (year == 2026 && month == 2) {
+        return "Spring Equinox March 20th";
+      } else if (year == 2026 && month == 5) {
+        return "Summer Solstice June 21st";
+      } else if (year == 2026 && month == 8) {
+        return "Fall Equinox: September 22nd";
+      } else if (year == 2026 && month == 11) {
+        return "Winter Solstice : December 21st";
+      } else if (year == 2027 && month == 2) {
+        return "Spring Equinox March 20th";
+      } else if (year == 2027 && month == 5) {
+        return "Summer Solstice June 21st";
+      } else if (year == 2027 && month == 8) {
+        return "Fall Equinox: September 22nd";
+      } else if (year == 2027 && month == 11) {
+        return "Winter Solstice : December 21st";
+      } else if (year == 2028 && month == 2) {
+        return "Spring Equinox March 19th";
+      } else if (year == 2028 && month == 5) {
+        return "Summer Solstice June 20th";
+      } else if (year == 2028 && month == 8) {
+        return "Fall Equinox: September 22nd";
+      } else if (year == 2028 && month == 11) {
+        return "Winter Solstice : December 21st";
+      } else if (year == 2029 && month == 2) {
+        return "Spring Equinox March 20th";
+      } else if (year == 2029 && month == 5) {
+        return "Summer Solstice June 20th";
+      } else if (year == 2029 && month == 8) {
+        return "Fall Equinox: September 22nd";
+      } else if (year == 2029 && month == 11) {
+        return "Winter Solstice : December 21st";
+      } else if (year == 2030 && month == 2) {
+        return "Spring Equinox March 20th";
+      } else if (year == 2030 && month == 5) {
+        return "Summer Solstice June 21st";
+      } else if (year == 2030 && month == 8) {
+        return "Fall Equinox: September 22nd";
+      } else if (year == 2030 && month == 11) {
+        return "Winter Solstice : December 21st";
+      } else {
+        return "Meteor Shower this Month!";
+      }
+    }
+
+    function planetType() {
+      if ((planetnews()).includes("Solstice") || (planetnews()).includes("Equinox")) {
+        return "Sun";
+      } else if ((planetnews()).includes("Meteor") || (planetnews()).includes("Shower")) {
+        return "Shower";
+      } else if ((planetnews()).includes("Mercury")) {
+        return "Mercury";
+      } else if ((planetnews()).includes("Venus")) {
+        return "Venus";
+      } else if ((planetnews()).includes("Mars")) {
+        return "Mars";
+      } else if ((planetnews()).includes("Jupiter")) {
+        return "Jupiter";
+      } else if ((planetnews()).includes("Saturn")) {
+        return "Saturn";
+      } else if ((planetnews()).includes("Uranus")) {
+        return "Uranus";
+      } else if ((planetnews()).includes("Neptune")) {
+        return "Neptune";
+      } else if ((planetnews()).includes("Pluto")) {
+        return "Pluto";
+      } else {
+        return "Shower";
+      }
+    }
+
+
     //Time Functions
 
     hr_rotation = 30 * hr + min / 2; //converting current time
@@ -507,7 +678,7 @@ window.onload = function() {
     second.style.transform = `rotate(${sec_rotation}deg)`;
 
     //Button to control Clock
-    if (buttonnumber > 4) {
+    if (buttonnumber > 5) {
       buttonnumber = 0;
     }
 
@@ -521,7 +692,12 @@ window.onload = function() {
         button.innerHTML = Holiday();
       } else if (buttonnumber == 4) {
         button.innerHTML = "Moon Phase: " + moonNames[getMoonPhase(year, month, day)];
-      } else {
+      }
+       else if (buttonnumber == 5) {
+        button.innerHTML = planetnews();
+      }
+      
+      else {
         button.innerHTML = "Clock Showing :  <" + hr + ":" + min + ":" + sec + ">";
       }
     };
@@ -548,7 +724,13 @@ window.onload = function() {
       }
     } else if (buttonnumber == 4) {
       star.style.backgroundImage = "url('https://github.com/SarahBass/SarahBass.github.io/blob/main/images/moon" + getMoonPhase(year, month, day) + ".png?raw=true')";
-    } else {
+    }
+    else if (buttonnumber == 5) {
+      star.style.backgroundImage = "url('https://github.com/SarahBass/SarahBass.github.io/blob/main/images/" + planetType() + ".png?raw=true')";
+    }
+    
+    
+    else {
       star.style.backgroundImage = "url('https://github.com/SarahBass/SarahBass.github.io/blob/main/images/" + min % 10 + "star" + sec % 2 + ".png?raw=true')";
     }
 
